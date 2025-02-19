110a4a37ff216f7509a38dd65e9d660b5b035539
Sqlitt <118855875+sqlitt@users.noreply.github.com>
2025-02-19 18:07:02 -0300

    created user in api

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,10 +1,11 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
-import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
+import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from "fastify-type-provider-zod";
 import { fastifySwagger } from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
+import { routes } from "./routes";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -19,16 +20,19 @@ app.register(fastifySwagger, {
             title: 'Duofinance API',
             version: '1.0.0',
         },
-    }
+    },
+    transform: jsonSchemaTransform
 });
 
 app.register(fastifySwaggerUi, {
     routePrefix: '/docs',
-})
+    uiConfig: {
+        docExpansion: 'list',
+        deepLinking: false,
+    },
+});
 
-app.get('/health', () => {
-    return 'ok'
-})
+app.register(routes)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running on http://localhost:3333')
